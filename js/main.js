6918336395b9c511c400fd278ec3e1b0764d4270
OBakir90 <53667329+OBakir90@users.noreply.github.com>
2020-06-03 13:56:08 +0100

    added new features

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+// Add to cart
+
 const mediaName = document.querySelectorAll('.media h5')
 const mediaPrice = document.querySelectorAll('.media span')
 const buttons = document.querySelectorAll('a.tm-buy')
@@ -8,18 +10,24 @@ const cartAmount = document.getElementById('cart_amount')
 const cartImg = document.querySelectorAll('.nav-item')[3]
 
 
-
-
-
 buttons.forEach((button, index) => {
     function clicked() {
-        cartProduct.innerText = mediaName[index].textContent
-        cartAmount.innerText = mediaPrice[index].textContent
+        const newRow = document.createElement('tr')
+        const newColumn1 = document.createElement('th')
+        const newColumn2 = document.createElement('th')
+        newColumn1.innerText = mediaName[index].textContent
+        newColumn2.innerText = mediaPrice[index].textContent
+        newRow.appendChild(newColumn1)
+        newRow.appendChild(newColumn2)
+        cart.querySelector('table').appendChild(newRow)
     }
     button.addEventListener('click', clicked)
 })
 
 cartImg.addEventListener('click', () => cart.className === 'cart-preview-passive' ? cart.className = 'cart-preview-active' : cart.className = 'cart-preview-passive')
+
+
+// Search Album
 
 const searchInput = document.querySelector('.tm-search-input')
 const albums = document.querySelectorAll('.tm-albums-container .tm-album-col')
@@ -37,3 +45,25 @@ function search(e) {
     })
 }
 searchInput.addEventListener('input', search)
+
+// Submit Email
+
+const navList = document.querySelector('.navbar-nav')
+const submitForm = document.querySelector('form')[1]
+const emailInput = document.querySelector('.tm-subscribe-input')
+const submitBtn = document.querySelector('.tm-subscribe-btn')
+const login = document.createElement('li')
+
+console.log(navList)
+
+function loginPage(e) {
+    e.preventDefault()
+    const email = emailInput.value
+    if (email.includes('@')) {
+        login.innerText = `logged in as ${emailInput.value}`
+        navList.appendChild(login)
+        window.scrollTo(500, 0)
+    } else { alert('please enter a valid email') }
+}
+
+submitBtn.addEventListener('click', loginPage)
