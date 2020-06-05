f663b01eea7e89787d2acf5807fe8272b0eaa50a
supertata <laetitiachira@gmail.com>
2020-06-05 11:23:39 +0100

    final 3 working features

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-//mouseover middle page rectangle and page background color turns blue
+//mouseover middle page rectangle and page background color turns beige
 
 let midPageRectangle = document.querySelector(".tm-tag-line");
 let changeBackgroundColor = () => document.body.style.backgroundColor = "beige";
 midPageRectangle.addEventListener('mouseover', changeBackgroundColor);
 
-//Click on buy button and an alert pops up //need to extend code to all buttons
+//Click on buy buttons and an alert pops up 
 
-let buyButtons = document.querySelector(".tm-buy-box");
-let createItemToBasketMessage = () => alert("Item added to your basket");
-buyButtons.addEventListener('click', createItemToBasketMessage);
+let buyButtons = document.querySelectorAll(".tm-buy-box");
+buyButtons.forEach((button) => {
+   function createMessage () {
+       alert("Item added to your basket");
+    }
+    button.addEventListener('click', createMessage);
+})
 
-//keyup input and generatas 'How are you'  //need to fix bug of prompt popping up at every keep pressed
+// prompt pop up after 5 seconds of page upload  
+document.onload = setTimeout(function () { prompt('Hello! Register to our Newsletter by entering your email address and receive 20% discount'); }, 5000);
+document.addEventListener("load", setTimeout);
 
-let searchButton = document.querySelector(".tm-search-input");
-let createHowAreYouPrompt = () => prompt("How are you today");
-searchButton.addEventListener("keyup", createHowAreYouPrompt, false);
 
 
 
